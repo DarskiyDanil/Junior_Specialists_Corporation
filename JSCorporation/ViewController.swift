2eb201d99789bbe8e308e6e1fc90db9640e853a7
Boris Pavlov <pbd_b@icloud.com>
2019-06-01 21:13:26 +0300

    Initial Commit

---
+++ JSCorporation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JSCorporation
+//
+//  Created by Борис Павлов on 01/06/2019.
+//  Copyright © 2019 Boris. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
